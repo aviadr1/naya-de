157fb61b296179f7f1fc36b4efe51f3b448f451c
Aviad Rozenhek <aviadr1@gmail.com>
2023-03-28 10:24:09 +0000

    fix Exresices

--- 05_spark/01_rdd/Exresices.py
+++ 05_spark/01_rdd/Exresices.py
@@ -7,17 +7,26 @@
 4. How many words include all 5 vowels?
 '''
 
+from pathlib import Path
 from pyspark import SparkContext
+from term_colors import *
 sc = SparkContext.getOrCreate()
+
+folder = Path(__file__).parent
+english_words_filename = str(folder / "english words.txt")
 
 # Read a text file
 text = sc\
-    .textFile(r"/tmp/pycharm_project_574/05_spark/spark_RDD/english words.txt")
+    .textFile(english_words_filename)
 
+print(h1('first 10 lines:'))
 print(text.take(10))
+print('\n')
 
 # 1. How many words are listed in the file?
+print(h1('number of words:'))
 print(text.count())
+print('\n')
 
 # 2. What is the most common first letter?
 text_rdd2 = text\
@@ -26,7 +35,9 @@ text_rdd2 = text\
     .mapValues(len)\
     .sortBy(lambda word:word[1], ascending= False)
 
+print(h1('most common first letter:'))
 print(text_rdd2.take(10))
+print('\n')
 
 
 #option 2
@@ -41,13 +52,18 @@ print(text_rdd2.take(10))
 # 3. What is the longest word in the file?
 
 word_len_max = text.map(lambda x: len(x)).max()
+print(h1('longest word length:'))
 print(word_len_max)
 longest_word = text.filter(lambda word: len(word)==word_len_max)
+print(h1('longest words:'))
 print(longest_word.take(60))
+print('\n')
 
 # 4. How many words include all 5 vowels?
 text_rdd= text.filter(lambda word: set("aeiuo")<set(word))
+print(h1('words with all 5 vowels:'))
 print(text_rdd.take(10))
+print('\n')
 
 
 
