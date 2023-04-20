4851d74d5f5a637d0e18b1f49943aa872e434e6f
Aviad Rozenhek <aviadr1@gmail.com>
2023-04-20 15:29:04 +0000

    black reformatting

--- 05_spark/04_structured/01_Example_02_tweeter_consumer.py
+++ 05_spark/04_structured/01_Example_02_tweeter_consumer.py
@@ -1,4 +1,4 @@
-'''
+"""
 1. ReadStream From tweets in Kafka topic  "TweeterData"
 2. Change the json to dataframe in spark
 3. Add current time in timestamp in column "current_ts"
@@ -6,8 +6,7 @@
 5. Add minute current time in column "minute"
 6. Filter just more than 10 words in tweets
 7. Print in Console
-'''
-
+"""
 
 
 # !pip install TextBlob
@@ -18,7 +17,9 @@ from textblob import TextBlob
 from pyspark.sql.types import StringType, StructType, IntegerType, FloatType
 
 # conection between  spark and kafka
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
+os.environ[
+    "PYSPARK_SUBMIT_ARGS"
+] = "--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell"
 
 bootstrapServers = "Cnt7-naya-cdh63:9092"
 topics = "TweeterData"
@@ -27,67 +28,57 @@ output_raw_kafka = False
 output_raw_tweets = False
 filter_short_tweets = True
 
-spark = SparkSession\
-        .builder\
-        .appName("ReadTweets")\
-        .getOrCreate()
+spark = SparkSession.builder.appName("ReadTweets").getOrCreate()
 
 # ReadStream from kafka
-df_kafka = spark\
-    .readStream\
-    .format("kafka")\
-    .option("kafka.bootstrap.servers", bootstrapServers)\
-    .option("subscribe", topics)\
+df_kafka = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", bootstrapServers)
+    .option("subscribe", topics)
     .load()
+)
 
 if output_raw_kafka:
     # just print the bare kafka structure
-    result = (df_kafka
-        .writeStream 
-        .format("console") 
-        .start()
-        .awaitTermination()
-    )
-    
+    result = df_kafka.writeStream.format("console").start().awaitTermination()
 
 
 # Create schema for create df from json
-schema = StructType() \
-    .add("tweet_created_at", StringType()) \
-    .add("tweet_id", StringType()) \
-    .add("text", StringType()) \
-    .add("user_acount_created_at", StringType()) \
-    .add("user_id", StringType())\
-    .add("name", StringType()) \
-    .add("followers_count", IntegerType()) \
-    .add("friends_count", IntegerType())  \
+schema = (
+    StructType()
+    .add("tweet_created_at", StringType())
+    .add("tweet_id", StringType())
+    .add("text", StringType())
+    .add("user_acount_created_at", StringType())
+    .add("user_id", StringType())
+    .add("name", StringType())
+    .add("followers_count", IntegerType())
+    .add("friends_count", IntegerType())
     .add("listed_count", IntegerType())
+)
 
 
 # change json to dataframe with schema
-df_tweets = df_kafka.select(col("value").cast("string"))\
-    .select(from_json(col("value"), schema).alias("value"))\
+df_tweets = (
+    df_kafka.select(col("value").cast("string"))
+    .select(from_json(col("value"), schema).alias("value"))
     .select("value.*")
+)
 
 if output_raw_tweets:
-    result = (df_tweets
-        .writeStream 
-        .format("console") 
-        .start()
-        .awaitTermination()
-    )
+    result = df_tweets.writeStream.format("console").start().awaitTermination()
 
 # Add current time in timestamp in column "current_ts"
-df_tweets = df_tweets.withColumn("current_ts", current_timestamp().cast('string'))
+df_tweets = df_tweets.withColumn("current_ts", current_timestamp().cast("string"))
 
 # Add hour current time  in column "hour"
-df_tweets = df_tweets.withColumn("hour", hour("current_ts").cast('integer'))
+df_tweets = df_tweets.withColumn("hour", hour("current_ts").cast("integer"))
 
 # Add minute current time in column "minute"
-df_tweets = df_tweets.withColumn("minute", minute("current_ts").cast('integer'))
+df_tweets = df_tweets.withColumn("minute", minute("current_ts").cast("integer"))
 
 # Add wordcount in column "wordCount"
-df_tweets = df_tweets.withColumn('wordCount', size(split(col('text'), ' ')))
+df_tweets = df_tweets.withColumn("wordCount", size(split(col("text"), " ")))
 
 if filter_short_tweets:
     # Filter just more than 10 words in tweets
@@ -97,11 +88,8 @@ if filter_short_tweets:
 def get_sentiment(string1):
     return TextBlob(string1).sentiment.polarity
 
-get_sentiment_udf = udf(get_sentiment, FloatType())
-df_tweets = df_tweets.withColumn('sentiment', get_sentiment_udf(col('text')))
 
-df_tweets \
-    .writeStream \
-    .format("console") \
-    .start()\
-    .awaitTermination()
+get_sentiment_udf = udf(get_sentiment, FloatType())
+df_tweets = df_tweets.withColumn("sentiment", get_sentiment_udf(col("text")))
+
+result = df_tweets.writeStream.format("console").start().awaitTermination()
