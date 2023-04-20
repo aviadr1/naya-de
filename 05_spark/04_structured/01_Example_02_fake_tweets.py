e3b0b8f5c00d6a0d8648412306ea118f1b415b80
Aviad Rozenhek <aviadr1@gmail.com>
2023-04-20 15:25:41 +0000

    black reformatting

--- 05_spark/04_structured/01_Example_02_fake_tweets.py
+++ 05_spark/04_structured/01_Example_02_fake_tweets.py
@@ -1,6 +1,6 @@
 #
 # for this example you need to install the faker library
-#   pip install faker
+#   pip install "faker==14.2.*"
 #
 import json
 from kafka import KafkaProducer
@@ -23,26 +23,25 @@ sweet_words = [
 ]
 
 producer = KafkaProducer(
-    bootstrap_servers='Cnt7-naya-cdh63:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+    bootstrap_servers="Cnt7-naya-cdh63:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 
 while True:
     user_events = {}
-    user_events['tweet_created_at'] = fake.date_time().ctime()
-    user_events['tweet_id'] = str(fake.ssn())
-    user_events['text'] = fake.sentence(ext_word_list=sweet_words, nb_words=10)
-    user_events['user_acount_created_at'] = fake.date_time().ctime()
-    user_events['user_id'] = str(fake.ssn())
-    user_events['name'] = fake.name()
-    user_events['followers_count'] = int(fake.random_number())
-    user_events['friends_count'] = int(fake.random_number())
-    user_events['listed_count'] = int(fake.random_number())
-    # user_events['location'] = fake.city()
-    
+    user_events["tweet_created_at"] = fake.date_time().ctime()
+    user_events["tweet_id"] = str(fake.ssn())
+    user_events["text"] = fake.sentence(ext_word_list=sweet_words, nb_words=10)
+    user_events["user_acount_created_at"] = fake.date_time().ctime()
+    user_events["user_id"] = str(fake.ssn())
+    user_events["name"] = fake.name()
+    user_events["followers_count"] = int(fake.random_number())
+    user_events["friends_count"] = int(fake.random_number())
+    user_events["listed_count"] = int(fake.random_number())
+    user_events["location"] = fake.city()
+
     pprint(user_events)
     print()
-    producer.send('TweeterData', user_events)
+    producer.send("TweeterData", user_events)
     producer.flush()
     time.sleep(random.random() * 4)
-
