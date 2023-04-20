5f553d1941691f59a737f99a6f19ee254b56d91a
Aviad Rozenhek <aviadr1@gmail.com>
2023-04-20 15:31:30 +0000

    black reformatting

--- 05_spark/04_structured/02_Solution.py
+++ 05_spark/04_structured/02_Solution.py
@@ -1,4 +1,4 @@
-'''
+"""
 Run the verse generator and send it to Kafka.
 Each file should have the following structure: {'chapter': i, 'verse': j, 'text': text}
 
@@ -9,7 +9,7 @@ Each file should have the following structure: {'chapter': i, 'verse': j, 'text'
 5. Add new column If the number of words greater than 14,”long”,if between  7 to 14, “medium”, otherwise “small”
 6. Remove the column with number of “God” in verse.
 
-'''
+"""
 
 
 import os
@@ -20,60 +20,64 @@ from pyspark.sql.types import StringType, StructType, IntegerType, FloatType
 from pyspark.sql import functions as F
 
 # conection between  spark and kafka
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
+os.environ[
+    "PYSPARK_SUBMIT_ARGS"
+] = "--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell"
 
 bootstrapServers = "Cnt7-naya-cdh63:9092"
 topics = "bible"
 
 
-spark = SparkSession\
-        .builder\
-        .appName("Readbible")\
-        .getOrCreate()
+spark = SparkSession.builder.appName("Readbible").getOrCreate()
 
 # ReadStream from kafka
-df_kafka = spark\
-    .readStream\
-    .format("kafka")\
-    .option("kafka.bootstrap.servers", bootstrapServers)\
-    .option("subscribe", topics)\
+df_kafka = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", bootstrapServers)
+    .option("subscribe", topics)
     .load()
+)
 
 df_kafka = df_kafka.select(col("value").cast("string"))
 
 
-schema = StructType() \
-    .add("chapter", StringType()) \
-    .add("verse", StringType()) \
+schema = (
+    StructType()
+    .add("chapter", StringType())
+    .add("verse", StringType())
     .add("text", StringType())
+)
 
 # change json to dataframe with schema
-df_kafka = df_kafka.select(col("value").cast("string"))\
-    .select(from_json(col("value"), schema).alias("value"))\
+df_kafka = (
+    df_kafka.select(col("value").cast("string"))
+    .select(from_json(col("value"), schema).alias("value"))
     .select("value.*")
+)
 
 # Add new column with number of “God” in verse.
-df_kafka = df_kafka.withColumn('GodCount', size(split(col('text'), 'God')) - 1)
+df_kafka = df_kafka.withColumn("GodCount", size(split(col("text"), "God")) - 1)
 
 # Filter only verses with number of “God” greater than 1
-df_kafka= df_kafka.where(col('GodCount')>1)
+df_kafka = df_kafka.where(col("GodCount") > 1)
 
 # Filter only verses that contain “and”
-df_kafka= df_kafka.filter(df_kafka.text.like('%and%'))
+df_kafka = df_kafka.filter(df_kafka.text.like("%and%"))
 
-df_kafka= df_kafka.withColumn('Counter_words', size(split(col('text'), ' ')) - 1)
+df_kafka = df_kafka.withColumn("Counter_words", size(split(col("text"), " ")) - 1)
 
 
 # Add new column If the number of words greater than 14,”long”,if between  7 to 14, “medium”, otherwise “small”
-df_kafka= df_kafka.withColumn('length', F.when(col('Counter_words')>14, "long").when(col('Counter_words')>7, 'medium').otherwise('small'))
+df_kafka = df_kafka.withColumn(
+    "length",
+    F.when(col("Counter_words") > 14, "long")
+    .when(col("Counter_words") > 7, "medium")
+    .otherwise("small"),
+)
 
 
 # Remove the column with number of “God” in verse
-df_kafka= df_kafka.drop('GodCount')
+df_kafka = df_kafka.drop("GodCount")
 
 
-df_kafka \
-    .writeStream \
-    .format("console") \
-    .start()\
-    .awaitTermination()
+df_kafka.writeStream.format("console").start().awaitTermination()
