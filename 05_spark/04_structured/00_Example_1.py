0eab3307c708e9bee6308bc047c2edc1e0b9ed31
Aviad Rozenhek <aviadr1@gmail.com>
2023-04-20 11:08:51 +0000

    improve example 00

--- 05_spark/04_structured/00_Example_1.py
+++ 05_spark/04_structured/00_Example_1.py
@@ -18,6 +18,11 @@ spark = SparkSession\
         .appName("Counter")\
         .getOrCreate()
 
+### Options for playing with variations of the pipeline. you can change values to True/False
+filter_words_with_i = False
+filter_minimum_letters = False
+drop_wordCount_column = False
+
 # 1. first, YOU need to run the program `nc -lk 9999` in the terminal to create a socket
 # 2. then YOU need to type numbers in the terminal
 # 3. NOW, this PROGRAM can read from socket
@@ -30,6 +35,7 @@ socketDF = spark \
 
 print(type(socketDF))
 print(socketDF)
+socketDF.printSchema()
 
 # Split the lines into words
 words = socketDF.select(
@@ -39,27 +45,30 @@ words = socketDF.select(
 )
 #https://sparkbyexamples.com/pyspark/pyspark-explode-array-and-map-columns-to-rows/
 
-# Filter to get only names with "i"
-words = words.filter(words.word.like('%i%'))
+if filter_words_with_i:
+    # Filter to get only names with "i"
+    words = words.filter(words.word.like('%i%'))
 
 # Change the first letter to uppercase
 def capitalize(s):
   return s[0].upper() + s[1:]
-
 capitalize_udf = udf(f=capitalize, returnType=StringType())
-
 
 words = words\
     .withColumn('word', capitalize_udf('word'))\
 #
 # # Add the number of letters in any word
-words = words.withColumn('wordCount', size(split(col('word'), ''))-1)
+words = words.withColumn('length', size(split(col('word'), ''))-1)
 
-# Take words with greater than 3 letters
-words = words.where(words.wordCount>3)
 
-# drop the word count
-words = words.drop('wordCount')
+if filter_minimum_letters:
+    # Take words with greater than 3 letters
+    words = words.where(words.wordCount>3)
+
+
+if drop_wordCount_column:
+    # drop the word count
+    words = words.drop('length')
 
 # send to console
 words \
