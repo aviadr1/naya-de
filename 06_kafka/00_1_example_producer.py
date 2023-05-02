4f1da705eb59bb36cb6309e2a916c82f5458b722
Aviad Rozenhek <aviadr1@gmail.com>
2023-05-02 16:24:33 +0000

    example 00 -> use history yes/no option

--- 06_kafka/00_1_example_producer.py
+++ 06_kafka/00_1_example_producer.py
@@ -1,6 +1,7 @@
 from kafka import KafkaProducer
 
 from time import sleep
+import time
 
 # Topics/Brokers
 topic1 = 'kafka-tst-01'
@@ -15,4 +16,8 @@ producer.flush()
 
 # # One more example
 producer.send(topic1, value=b'This is a Kafka-Python basic tutorial')
+producer.flush()
+
+
+producer.send(topic1, value=f'Aviad was here {time.time()}'.encode('utf-8'))
 producer.flush()
--- 06_kafka/00_2_example_1_consumer.py
+++ 06_kafka/00_2_example_1_consumer.py
@@ -5,9 +5,22 @@ from kafka import KafkaConsumer
 topic1 = 'kafka-tst-01'
 brokers = ['cnt7-naya-cdh63:9092']
 
+use_past_data = False
 # First we set the consumer,
 # and we use the KafkaConsumer class to create a generator of the messages.
-consumer = KafkaConsumer(topic1,bootstrap_servers=brokers)
+if use_past_data:
+    consumer = KafkaConsumer(
+        topic1,
+        bootstrap_servers=brokers,
+        auto_offset_reset='earliest', #
+        enable_auto_commit=True,
+        auto_commit_interval_ms=1000
+        )
+else:
+    consumer = KafkaConsumer(
+        topic1,
+        bootstrap_servers=brokers
+    )
 
 # print the value of the consumer
 # we run the consumer generator to fetch the message scoming from topic1.
