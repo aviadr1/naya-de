4fe34f12d30dde027d792e4d94f94e299c2c93f9
Aviad Rozenhek <aviadr1@gmail.com>
2023-05-02 16:45:06 +0000

    add ability to truncate the SQL table before loading

--- 06_kafka/01_5_mysql_connector_create.py
+++ 06_kafka/01_5_mysql_connector_create.py
@@ -12,6 +12,8 @@ mysql_table_name = 'src_events'
 mysql_username = 'naya'
 mysql_password = 'NayaPass1!'
 
+truncate = False
+
 # connector to mysql
 mysql_conn = mc.connect(user=mysql_username,
                         password=mysql_password,
@@ -23,9 +25,11 @@ mysql_cursor= mysql_conn.cursor()
 # create database if needed
 mysql_cursor.execute("create database if not exists srcdb;")
 
+if truncate:
+   mysql_cursor.execute("TRUNCATE TABLE srcdb.kafka_pipeline")
+   print('truncated table!')
 
 # Create table if needed
-
 mysql_create_tbl_events = '''create table if not exists srcdb.kafka_pipeline
     (event_id varchar(10) primary key,
      hostname varchar(30),
