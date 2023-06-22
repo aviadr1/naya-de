2daa4926dbf8e03d62f0e1333d75588163d98e33
Aviad Rozenhek <aviadr1@gmail.com>
2023-06-22 17:05:08 +0300

    Update setup_python.sh

--- 00_scripts/setup_python.sh
+++ 00_scripts/setup_python.sh
@@ -15,13 +15,13 @@ sudo yum install netcat
 # which is compatible with the installation of hadoop, spark and kafka 
 conda activate
 conda update conda
-conda install python=3.6.6 pyhive=0.6.1 thriftpy2 sqlalchemy=1.3.1 ibis-framework=0.14.0 mysql-connector-python pyspark=2.4.3 
-conda install -c conda-forge matplotlib
-conda install -c conda-forge tweepy
+conda install --yes python=3.6.6 pyhive=0.6.1 thriftpy2 sqlalchemy=1.3.1 ibis-framework=0.14.0 mysql-connector-python pyspark=2.4.3 
+conda install --yes -c conda-forge matplotlib
+conda install --yes -c conda-forge tweepy
 pip install textblob
 conda config --append channels conda-forge
-conda install kafka-python=1.4.6
-conda install mysql-connector-python
+conda install --yes kafka-python=1.4.6
+conda install --yes mysql-connector-python
 
 
 
